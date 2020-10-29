e3303614a5140ebf5d65962f5fc29aec74258978
Luis Angel <santosangel748@gmail.com>
2020-10-29 13:07:41 -0600

    Mostra notificaciones al usuario

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UsuarioModel } from '../../models/usuario.model';
 import { AuthService } from '../../services/auth.service';
+import swal from 'sweetalert';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +14,8 @@ export class LoginComponent implements OnInit {
 
   usuario: UsuarioModel = new UsuarioModel();
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
   }
@@ -20,11 +23,27 @@ export class LoginComponent implements OnInit {
   login(form: NgForm){
     if (form.invalid) { return; }
 
+    swal({
+      button: false,
+      icon: 'info',
+      text: 'Espere por favor...',
+      closeOnClickOutside: false,
+    });
+
     this.auth.login(this.usuario)
     .subscribe(resp => {
       console.log(resp);
+      swal.close();
+      this.router.navigateByUrl('/home');
     }, (err) => {
-      console.log(err.error.error.message)
+      console.log(err.error.error.message);
+      swal({
+        icon: 'error',
+        title: 'Error al autenticar',
+        text: err.error.error.message,
+        
+        //timer: 3000,
+      });
     });
   }
 
--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UsuarioModel } from '../../models/usuario.model';
 import { AuthService } from '../../services/auth.service';
+import swal from 'sweetalert';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -12,7 +14,8 @@ export class RegistroComponent implements OnInit {
 
   usuario: UsuarioModel;
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
     this.usuario = new UsuarioModel();
@@ -22,11 +25,25 @@ export class RegistroComponent implements OnInit {
   onSubmit(form: NgForm ) {
     if (form.invalid) { return; }
 
+    swal({
+      button: false,
+      icon: 'info',
+      text: 'Espere por favor...',
+      closeOnClickOutside: false,
+    });
+
     this.auth.nuevousuario(this.usuario)
     .subscribe(resp => {
       console.log(resp);
+      swal.close();
+      this.router.navigateByUrl('/home');
     }, (err) => {
       console.log(err.error.error.message);
+      swal({
+        icon: 'error',
+        title: 'Error al registrar',
+        text: err.error.error.message
+      });
     });
   }
 
