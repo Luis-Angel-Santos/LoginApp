21c5448f282473d091acb00c90f54164603e1596
Luis Angel <santosangel748@gmail.com>
2020-10-29 12:27:08 -0600

    Guardar token de forma local

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UsuarioModel } from '../models/usuario.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,17 +11,21 @@ export class AuthService {
   private url = 'https://identitytoolkit.googleapis.com/v1';
   private apikey = 'AIzaSyCcbrxjve0FzI4tXRmjW7BkNA8qfIUQ1HE';
 
+  userToken: string;
+
   //https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
   //https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
 
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.leerToken();
+  }
 
   logout(){
 
   }
 
-  login(usuario: UsuarioModel){
+  login(usuario: UsuarioModel) {
     const authData = {
       ...usuario,
       returnSecureToken: true
@@ -29,7 +34,14 @@ export class AuthService {
     return this.http.post(
       `${this.url}/accounts:signInWithPassword?key=${this.apikey}`,
       authData
+    ).pipe(
+      map(resp => {
+        console.log('map');
+        this.guardarToken(resp['idToken']);
+        return resp;
+      })
     );
+
   }
 
   nuevousuario(usuario: UsuarioModel){
@@ -41,7 +53,28 @@ export class AuthService {
     return this.http.post(
       `${this.url}/accounts:signUp?key=${this.apikey}`,
       authData
+    ).pipe(
+      map(resp => {
+        console.log('map');
+        this.guardarToken(resp['idToken']);
+        return resp;
+      })
     );
   }
 
+  private guardarToken(idToken: string){
+    this.userToken = idToken;
+    localStorage.setItem('token', idToken);
+  }
+
+
+  leerToken(){
+    if (localStorage.getItem('token')) {
+      this.userToken = localStorage.getItem('token');
+    } else {
+      this.userToken = '';
+    }
+
+    return this.userToken;
+  }
 }
